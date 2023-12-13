d5dab664b3536512b21ac917a28924e0c7e2f80f
CalumHd <calum.hoad@ed.ac.uk>
2023-12-13 17:23:05 +0000

    Working on script

--- scripts/r/ls-ndvi-metrics.R
+++ scripts/r/ls-ndvi-metrics.R
@@ -15,95 +15,103 @@ library(viridis)
 #library(janitor)
 
 # Create a list of the S2 R10m files for each S2 scene
-d20230608 <- list.files('../../data/landsat-imagery/LC08_L2SP_011011_20230608_20230614_02_T1/', pattern = "*_B[0-9]+\\.tif$", full.names = TRUE)
-d20230702 <- list.files('../../data/sentinel-2/20230708/S2A_MSIL2A_20230708T152811_N0509_R111_T21WXT_20230708T214952.SAFE/GRANULE/L2A_T21WXT_A042007_20230708T152945/IMG_DATA/R10m/', full.names = TRUE)
-d20230708 <- list.files('../../data/sentinel-2/20230726/S2B_MSIL2A_20230726T153819_N0509_R011_T21WXT_20230726T184037.SAFE/GRANULE/L2A_T21WXT_A033356_20230726T153828/IMG_DATA/R10m/', full.names = TRUE)
-d20230726 <- list.files('../../data/sentinel-2/20230729/S2B_MSIL2A_20230729T154819_N0509_R054_T21WXT_20230729T181552.SAFE/GRANULE/L2A_T21WXT_A033399_20230729T154940/IMG_DATA/R10m/', full.names = TRUE)
-d20230801 <- list.files('../../data/sentinel-2/20230807/S2A_MSIL2A_20230807T152811_N0509_R111_T21WXS_20230807T212701.SAFE/GRANULE/L2A_T21WXS_A042436_20230807T153242/IMG_DATA/R10m/', full.names = TRUE)
-d20230817 <- list.files('../../data/sentinel-2/20230808/S2B_MSIL2A_20230808T154819_N0509_R054_T21WXS_20230914T102422.SAFE/GRANULE/L2A_T21WXS_A033542_20230808T154852/IMG_DATA/R10m/', full.names = TRUE)
-d20230910 <- list.files('../../data/sentinel-2/20230817/S2A_MSIL2A_20230817T152941_N0509_R111_T21WXT_20230817T214159.SAFE/GRANULE/L2A_T21WXT_A042579_20230817T153311/IMG_DATA/R10m/', full.names = TRUE)
-d20230920 <- list.files('../../data/sentinel-2/20230923/S2A_MSIL2A_20230922T154951_N0509_R054_T21WXT_20230922T234400.SAFE/GRANULE/L2A_T21WXT_A043094_20230922T155005/IMG_DATA/R10m/', full.names = TRUE)
-
-ls <- rast(d20230608)
-
-d20230608
+d20230608 <- list.files('../../data/landsat-imagery/LC08_L2SP_011011_20230608_20230614_02_T1/', pattern = ".*_B[0-9]+\\.TIF$", full.names = TRUE)
+d20230702 <- list.files('../../data/landsat-imagery/LC09_L2SP_011011_20230702_20230704_02_T1/', pattern = ".*_B[0-9]+\\.TIF$", full.names = TRUE)
+d20230708 <- list.files('../../data/landsat-imagery/LC08_L2SP_013011_20230708_20230718_02_T1/', pattern = ".*_B[0-9]+\\.TIF$", full.names = TRUE)
+d20230726 <- list.files('../../data/landsat-imagery/LC08_L2SP_011011_20230726_20230805_02_T1/', pattern = ".*_B[0-9]+\\.TIF$", full.names = TRUE)
+d20230801 <- list.files('../../data/landsat-imagery/LC09_L2SP_013011_20230801_20230803_02_T1/', pattern = ".*_B[0-9]+\\.TIF$", full.names = TRUE)
+d20230817 <- list.files('../../data/landsat-imagery/LC09_L2SP_013011_20230817_20230822_02_T1/', pattern = ".*_B[0-9]+\\.TIF$", full.names = TRUE)
+d20230910 <- list.files('../../data/landsat-imagery/LC08_L2SP_013011_20230910_20230918_02_T1/', pattern = ".*_B[0-9]+\\.TIF$", full.names = TRUE)
+d20230920 <- list.files('../../data/landsat-imagery/LC09_L2SP_011011_20230920_20230922_02_T1/', pattern = ".*_B[0-9]+\\.TIF$", full.names = TRUE)
 
 # List of imagery dates, for later use
-dates <- c('2023-06-26', 
-           '2023-07-08',
+dates <- c('2023-06-08', 
+           '2023-07-02',
+           '2023-07-08', 
            '2023-07-26', 
-           '2023-07-29', 
-           '2023-08-07', 
-           '2023-08-08', 
+           '2023-08-01', 
            '2023-08-17', 
-           '2023-09-23')
+           '2023-09-10', 
+           '2023-09-20')
 
 # Get the Sentinel-2 10m bands as raster stack, project + crop to extent of UAV imagery
 # As function?
 
 # Create list where each item in the list is another list,
 #   containing the filepath to each imagery band
-s2.data <- list(d20230626, 
-                d20230708, 
-                d20230726, 
-                d20230729, 
-                d20230807, 
-                d20230808, 
-                d20230817, 
-                d20230923)
+ls.data <- list(d20230608,
+                d20230702,
+                d20230708,
+                d20230726,
+                d20230801,
+                d20230817,
+                d20230910,
+                d20230920) 
 
+crs(ls, proj = TRUE, describe = FALSE)
 # Get uAV imagery over plot to use for cropping - RE-EXPORT UAV IMAGERY SO RE-PROJECT IS AVOIDED
 uav <- project(rast('../../data/uav/M3M-exports/5cm/20230702-clipped-5cm-div128.tif'), 'epsg:32621')
 
 # Create function for stacking rasters from lists of filepaths, then cropping to extent of UAV imagery
-import_s2 <- function(x) {
-  x <- rast(x) %>%
-    crop(uav)
+import_ls <- function(x, rast.targ.proj) {
+  x <- rast(x)
+  if(crs(x, proj = TRUE, describe = FALSE) == "+proj=utm +zone=22 +datum=WGS84 +units=m +no_defs") {
+    print(paste0('Reprojecting', x, 'to UTM21N'))
+    x <- project(x, rast.targ.proj)
+  }
+  x <- x %>% crop(uav)
   #x <- set.names(object = x, nm = c('aot', 'blue', 'green', 'red', 'nir', 'tci1', 'tci2', 'tci3', 'wvp'))
 }
 
+# Create spatRast object for one Landsat image with desired projection
+target.rast <- rast(d20230708)
+# Check the projection of the target raster is correct
+crs(target.rast, proj = TRUE, describe = FALSE)
+
 # Import the data
-s2.data.import <- lapply(s2.data, import_s2)
+ls.data.import <- lapply(ls.data, import_ls, rast.targ.proj = target.rast)
 
 # Check the function worked by plotting rasters from list
-plot(s2.data.import[[6]])
+plot(ls.data.import[[8]])
+
+ls.data.import
 
 # Rename bands to make reading logical
-names(s2.data.import[[1]]) <- c('aot', 'blue', 'green', 'red', 'nir', 'tci1', 'tci2', 'tci3', 'wvp')
-names(s2.data.import[[2]]) <- c('aot', 'blue', 'green', 'red', 'nir', 'tci1', 'tci2', 'tci3', 'wvp')
-names(s2.data.import[[3]]) <- c('aot', 'blue', 'green', 'red', 'nir', 'tci1', 'tci2', 'tci3', 'wvp')
-names(s2.data.import[[4]]) <- c('aot', 'blue', 'green', 'red', 'nir', 'tci1', 'tci2', 'tci3', 'wvp')
-names(s2.data.import[[5]]) <- c('aot', 'blue', 'green', 'red', 'nir', 'tci1', 'tci2', 'tci3', 'wvp')
-names(s2.data.import[[6]]) <- c('aot', 'blue', 'green', 'red', 'nir', 'tci1', 'tci2', 'tci3', 'wvp')
-names(s2.data.import[[7]]) <- c('aot', 'blue', 'green', 'red', 'nir', 'tci1', 'tci2', 'tci3', 'wvp')
-names(s2.data.import[[8]]) <- c('aot', 'blue', 'green', 'red', 'nir', 'tci1', 'tci2', 'tci3', 'wvp')
+names(ls.data.import[[1]]) <- c('aot', 'blue', 'green', 'red', 'nir', 'swir1', 'swir2', 'b10')
+names(ls.data.import[[2]]) <- c('aot', 'blue', 'green', 'red', 'nir', 'swir1', 'swir2', 'b10')
+names(ls.data.import[[3]]) <- c('aot', 'blue', 'green', 'red', 'nir', 'swir1', 'swir2', 'b10')
+names(ls.data.import[[4]]) <- c('aot', 'blue', 'green', 'red', 'nir', 'swir1', 'swir2', 'b10')
+names(ls.data.import[[5]]) <- c('aot', 'blue', 'green', 'red', 'nir', 'swir1', 'swir2', 'b10')
+names(ls.data.import[[6]]) <- c('aot', 'blue', 'green', 'red', 'nir', 'swir1', 'swir2', 'b10')
+names(ls.data.import[[7]]) <- c('aot', 'blue', 'green', 'red', 'nir', 'swir1', 'swir2', 'b10')
+names(ls.data.import[[8]]) <- c('aot', 'blue', 'green', 'red', 'nir', 'swir1', 'swir2', 'b10')
 
 
 # Apply function to calculate NDVI ----
-s2_ndvi <- function(x) {
+ls_ndvi <- function(x) {
   x <- (x$nir-x$red)/(x$nir+x$red)
   # names(x) <- c('ndvi')
 }
 
-s2.ndvi <- lapply(s2.data.import, s2_ndvi)
+ls.ndvi <- lapply(ls.data.import, ls_ndvi)
 
 # Stack NDVI rasters into single spatRast
-s2.ndvi <- rast(s2.ndvi)
+ls.ndvi <- rast(ls.ndvi)
 
 # Name spatRaster layers with dates
-names(s2.ndvi) <- dates
+names(ls.ndvi) <- dates
 
-plot(s2.ndvi)
+plot(ls.ndvi)
 
 # Extract raster time series to points
-s2.ndvi.points <- st_as_sf(as.points(s2.ndvi, values = TRUE)) %>%
+ls.ndvi.points <- st_as_sf(as.points(ls.ndvi, values = TRUE)) %>%
   mutate(id = row_number())
 
 
 # Apply parabolic 2nd order polynomial to every pixel in the df ----
 
 # Get a dataframe of points from the raster
-s2.ndvi.long <- s2.ndvi.points %>%
+ls.ndvi.long <- ls.ndvi.points %>%
   pivot_longer(!geometry & !id, names_to = 'doy', values_to = 'ndvi') %>%
   mutate(doy = as.Date(doy)) %>%
   group_by(id)
@@ -153,20 +161,62 @@ model_ndvi <- function(data) {
 # Following tutorial here: https://data-se.netlify.app/2018/12/10/new-split-apply-combine-variant-in-dplyr-group-split/
 
 # Apply model_ndvi to data using group_modify
-s2.modelled.ndvi <- s2.ndvi.long %>%
+ls.modelled.ndvi <- ls.ndvi.long %>%
   group_modify(~ model_ndvi(.x)) %>%
-  mutate(ndvi.max.doy = as_date(ndvi.max.doy))
+  mutate(ndvi.max.date = as_date(ndvi.max.doy),
+         # Calculate fractional day of year, by adding decimal to date
+         ndvi.max.doy = yday(ndvi.max.date) + (ndvi.max.doy - floor(ndvi.max.doy)))
 
 # Plotting ----
-ggplot(s2.modelled.ndvi, aes(group_by = id)) +
+ggplot(ls.modelled.ndvi, aes(group_by = id)) +
   geom_line(aes(x = doy, y = ndvi.pred, group = id, color = ndvi.max)) +
   geom_point(aes(x = ndvi.max.doy, y = ndvi.max)) +
   scale_color_viridis()
 
 # Outputs ---- 
-s2.modelled.export <- s2.modelled.ndvi %>%
+
+# Wide format
+ls.modelled.export.wide <- ls.modelled.ndvi %>%
   group_by(id) %>%
   filter(row_number() == 1) %>%
   select(!doy & !ndvi & !ndvi.pred)
 
-st_write(st_as_sf(s2.modelled.export),  '../../data/sentinel-2/output/s2_modelled_point.shp')
+st_write(st_as_sf(ls.modelled.export.wide),  '../../data/sentinel-2/output/ls_modelled_point_wide.shp')
+write.csv2(ls.modelled.export.wide,  '../../data/sentinel-2/output/ls_modelled_point_wide.csv')
+
+# Long format
+ls.modelled.export.long <- ls.modelled.ndvi %>%
+  rename(doy.obs = 'doy', 
+         ndvi.obs = 'ndvi')
+
+st_write(st_as_sf(ls.modelled.export.long),  '../../data/sentinel-2/output/ls_modelled_point_long.shp')
+write.csv2(ls.modelled.export.long,  '../../data/sentinel-2/output/ls_modelled_point_long.csv')
+
+
+# Output the stacked landsat rasters for future use:
+writeRaster(ls.data.import[[1]], '../../data/landsat-imagery/terra/2023-06-08.tif')
+writeRaster(ls.data.import[[2]], '../../data/landsat-imagery/terra/2023-07-02.tif')
+writeRaster(ls.data.import[[3]], '../../data/landsat-imagery/terra/2023-07-08.tif')
+writeRaster(ls.data.import[[4]], '../../data/landsat-imagery/terra/2023-07-26.tif')
+writeRaster(ls.data.import[[5]], '../../data/landsat-imagery/terra/2023-08-01.tif')
+writeRaster(ls.data.import[[6]], '../../data/landsat-imagery/terra/2023-08-17.tif')
+writeRaster(ls.data.import[[7]], '../../data/landsat-imagery/terra/2023-09-10.tif')
+writeRaster(ls.data.import[[8]], '../../data/landsat-imagery/terra/2023-09-20.tif')
+
+dates <- c('2023-06-08', 
+           '2023-07-02',
+           '2023-07-08', 
+           '2023-07-26', 
+           '2023-08-01', 
+           '2023-08-17', 
+           '2023-09-10', 
+           '2023-09-20')
+
+names(ls.data.import[[1]]) <- c('aot', 'blue', 'green', 'red', 'nir', 'swir1', 'swir2', 'b10')
+names(ls.data.import[[2]]) <- c('aot', 'blue', 'green', 'red', 'nir', 'swir1', 'swir2', 'b10')
+names(ls.data.import[[3]]) <- c('aot', 'blue', 'green', 'red', 'nir', 'swir1', 'swir2', 'b10')
+names(ls.data.import[[4]]) <- c('aot', 'blue', 'green', 'red', 'nir', 'swir1', 'swir2', 'b10')
+names(ls.data.import[[5]]) <- c('aot', 'blue', 'green', 'red', 'nir', 'swir1', 'swir2', 'b10')
+names(ls.data.import[[6]]) <- c('aot', 'blue', 'green', 'red', 'nir', 'swir1', 'swir2', 'b10')
+names(ls.data.import[[7]]) <- c('aot', 'blue', 'green', 'red', 'nir', 'swir1', 'swir2', 'b10')
+names(ls.data.import[[8]]) <- c('aot', 'blue', 'green', 'red', 'nir', 'swir1', 'swir2', 'b10')
