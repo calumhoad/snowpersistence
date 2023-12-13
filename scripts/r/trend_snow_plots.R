3691a36b2ba231ff99945803434b3eeef71a2ac7
CalumHd <calum.hoad@ed.ac.uk>
2023-12-13 11:21:40 +0000

    Exploratory plotting of S2 snow and NDVI data

--- scripts/r/trend_snow_plots.R
+++ scripts/r/trend_snow_plots.R
@@ -10,25 +10,36 @@ library(sf)
 library(tidyverse)
 library(lubridate)
 library(patchwork)
+library(cowplot)
 
-# Read in the data
+# Read in the data ----
+
+# Trend data from LandsatTS, with automatic screening of pixels
 trends.auto <- read.csv2('../../data/lsatTS-output/blaesedalen/blaesedalen_auto_7yr_trnds.csv')
+
+# Trend data from LandsatTS, with manual screening of pixels
 trends.manual <- read.csv2('../../data/lsatTS-output/blaesedalen/blaesedalen_manual_7yr_trnds.csv')
+
+# Growing season data from LandsatTS, with automatic screening of pixels
 gs.auto <- read.csv2('../../data/lsatTS-output/blaesedalen/blaesedalen_auto_7yr_gs_metric.csv')
+
+# Growing season data from LandsatTS, with manual screening of pixels
 gs.manual <- read.csv2('../../data/lsatTS-output/blaesedalen/blaesedalen_manual_7yr_gs_metric.csv')
 
-view(gs.auto)
-
+# Landsat (30m) snow cover data, derived from UAV imagery
 ls.snow <- read.csv2('../../data/uav/snow-metrics/blaesedalen_30m_snowcover.csv') %>%
   rename(sample.id = sample_id)
 
+# Sentinel-2 (30,) snow cover data, drived from UAV imagery
 s2.snow <- read.csv2('../../data/uav/snow-metrics/blaesedalen_10m_snowcover.csv') %>%
   rename(ndvi.max = 'ndvi_mx', ndvi.max.doy = 'ndv_mx_') %>%
   mutate(ndvi.max.doy = yday(ndvi.max.doy))
+
+# Join snow and NDVI metrics, if separate datasets ----
 # Join data frames based on sample_id
 joined.auto <- left_join(gs.auto, ls.snow, by = "sample.id")
 
-# Filter gs datasets to only 2023
+# Filter gs datasets to only 2023 and max.ndvi.doy greater than 175
 gs.auto <- gs.auto %>% filter(year == 2023 & ndvi.max.doy > 175)
 gs.auto.joined <- left_join(gs.auto, snow, by = 'sample.id')
 
@@ -38,20 +49,40 @@ gs.auto.joined <- left_join(gs.auto, snow, by = 'sample.id')
 s2.max.ndvi.doy.plot <- ggplot(s2.snow, aes(x = ndvi.max.doy, y = snow.persist)) +
   geom_point(aes(color = ndvi.max)) +
   geom_smooth(method = 'lm') +
-  scale_color_viridis_c(name = "ndvi.max")
+  scale_color_viridis_c(name = "ndvi.max") +
+  labs(x = 'Maximum NDVI Day of Year', 
+       y = 'Snow persistence\n(unweighted average)', 
+       padding = 1) +
+  theme_cowplot()
 
 # S2 max ndvi against snow persistence
 s2.max.ndvi.plot <- ggplot(s2.snow, aes(x = ndvi.max, y = snow.persist)) +
   geom_point(aes(color = ndvi.max.doy)) +
   geom_smooth(method = 'lm') +
-  scale_color_viridis_c(name = "ndvi.max.doy") 
+  scale_color_viridis_c(name = "ndvi.max.doy") +
+  labs(x = 'Maximum NDVI', 
+       y = 'Snow persistence\n(unweighted average)', 
+       padding = 1) +
+  theme_cowplot()
+  
+s2.ndvi.metrics.plot <- ggplot(drop_na(s2.snow, snow.persist), aes(x = ndvi.max.doy, 
+                                            y = ndvi.max)) +
+  geom_point(alpha = 0.5, aes(color = snow.persist, size = snow.persist)) +
+  #geom_smooth(method = 'lm') +
+  scale_color_viridis_c(name = "snow.persist") +
+  labs(x = 'Maximum NDVI Day of Year', 
+       y = 'Matximum NDVI', 
+       padding = 1) +
+  theme_cowplot()
 
 # Arrange plots side by side
-plots_combined <- s2.max.ndvi.plot + s2.max.ndvi.doy.plot +
+plots_combined <- s2.max.ndvi.plot + 
+  s2.max.ndvi.doy.plot +
+  s2.ndvi.metrics.plot +
   plot_layout(ncol = 2)
 
 plots_combined
-
+  
 
 
 
