33c7d2148d147c9401cdc646af4839be8d3c9201
CalumHd <calum.hoad@ed.ac.uk>
2024-01-05 12:33:21 +0000

    read in data and joined to single df

--- scripts/r/s2-ndvi-metrics-comparison.R
+++ scripts/r/s2-ndvi-metrics-comparison.R
@@ -13,14 +13,34 @@ s2.para <- read.csv2('../../data/sentinel-2/output/s2_modelled_point_long.csv') 
     select(id, geometry, ndvi.max, ndvi.max.doy) %>%
     rename(geometry.p = 'geometry', 
            ndvi.max.p = 'ndvi.max',
-           ndvi.max.doy.p = 'ndvi.max.doy')
+           ndvi.max.doy.p = 'ndvi.max.doy') %>%
+    mutate(ndvi.max.doy.p = floor(ndvi.max.doy.p))
 
 # Smoothed spline
-s2.smooth <- read.csv2('../../data/sentinel-2/output/s2_modelled_JA_point_wide.csv')
+s2.smooth <- read.csv2('../../data/sentinel-2/output/s2_modelled_JA_point_wide.csv') %>%
+  select(id, geometry, ndvi.max, ndvi.max.doy) %>%
+  rename(geometry.s = 'geometry', 
+         ndvi.max.s = 'ndvi.max', 
+         ndvi.max.doy.s = 'ndvi.max.doy')
 
 # Beck double logistic
-s2.beck <- read.csv2('../../data/sentinel-2/output/s2_modelled_beck_point_wide.csv')
+s2.beck <- read.csv('../../data/sentinel-2/output/s2_modelled_beck_point_wide.csv') %>%
+  select(id, X, Y, ndvi.max, ndvi.max.doy) %>%
+  rename(geometry.x.b = 'X',
+         geometry.y.b = 'Y', 
+         ndvi.max.b = 'ndvi.max', 
+         ndvi.max.doy.b = 'ndvi.max.doy')
+
 
 # Join the data from the various model outputs ----
+s2.all <- left_join(s2.para, s2.smooth, by = 'id')
+s2.all <- left_join(s2.all, s2.beck, by = 'id')
+
+# Manual check of geometry values shows that all ids are consistent, drop geom 
+# columns which are redundant
+s2.all <- s2.all %>%
+  select(-geometry.p, -geometry.s) %>%
+  st_as_sf(coords = c("geometry.x.b", "geometry.y.b"), crs = 32621)
+
 
 # Compare and plot the data ----
