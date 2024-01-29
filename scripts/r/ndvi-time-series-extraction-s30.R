e10adfca558388198708f0afa757f8dfdb762a3b
CalumHd <calum.hoad@ed.ac.uk>
2024-01-29 17:28:33 +0000

    re-writing S30 NDVI time series extraction script

---
+++ scripts/r/ndvi-time-series-extraction-s30.R
@@ -0,0 +1,341 @@
+# NASA HLS 30m NDVI metrics
+# Calum Hoad, 29 Jan 2024
+
+# Extract NDVI time series from NASA HLS S30 data, over Kluane
+# High, Kluane Low, and Blaesedalen plot locations.
+
+# Import necessary libraries
+library(terra)
+library(dplyr)
+library(rts)
+library(ggplot2)
+library(buffeRs)
+library(sf)
+library(tidyverse)
+library(purrr)
+library(broom)
+library(viridis)
+library(pbapply)
+library(tidyterra)
+#library(janitor)
+
+###
+# NASA HLS S30
+###
+
+# Blaesedalen ###
+
+# Create a list of the S2 R10m files for each S2 scene
+d20230406 <- list.files('../../data/nasa-hls/blaesedalen/s30/2023-04-06/', full.names = TRUE)
+d20230501 <- list.files('../../data/nasa-hls/blaesedalen/s30/2023-05-01/', full.names = TRUE)
+d20230516 <- list.files('../../data/nasa-hls/blaesedalen/s30/2023-05-16/', full.names = TRUE)
+d20230522 <- list.files('../../data/nasa-hls/blaesedalen/s30/2023-05-22/', full.names = TRUE)
+d20230608 <- list.files('../../data/nasa-hls/blaesedalen/s30/2023-06-08/', full.names = TRUE)
+d20230626 <- list.files('../../data/nasa-hls/blaesedalen/s30/2023-06-26/', full.names = TRUE)
+d20230708 <- list.files('../../data/nasa-hls/blaesedalen/s30/2023-07-08/', full.names = TRUE)
+d20230726 <- list.files('../../data/nasa-hls/blaesedalen/s30/2023-07-26/', full.names = TRUE)
+d20230729 <- list.files('../../data/nasa-hls/blaesedalen/s30/2023-07-29/', full.names = TRUE)
+d20230807 <- list.files('../../data/nasa-hls/blaesedalen/s30/2023-08-07/', full.names = TRUE)
+d20230808 <- list.files('../../data/nasa-hls/blaesedalen/s30/2023-08-08/', full.names = TRUE)
+d20230914 <- list.files('../../data/nasa-hls/blaesedalen/s30/2023-09-14/', full.names = TRUE)
+d20230922 <- list.files('../../data/nasa-hls/blaesedalen/s30/2023-09-22/', full.names = TRUE)
+d20231003 <- list.files('../../data/nasa-hls/blaesedalen/s30/2023-10-03/', full.names = TRUE)
+
+# List of imagery dates, for later use
+bl.dates <- c('2023-04-06',
+           '2023-05-01', 
+           '2023-05-16',
+           '2023-05-22', 
+           '2023-06-08', 
+           '2023-06-26', 
+           '2023-07-08', 
+           '2023-07-26', 
+           '2023-07-29', 
+           '2023-08-07', 
+           '2023-08-08', 
+           '2023-09-14', 
+           '2023-09-22', 
+           '2023-10-03')
+
+# Create list where each item in the list is another list,
+#   containing the filepath to each imagery band
+s30.bl.data <- list(d20230406,
+                 d20230501,
+                 d20230516,
+                 d20230522,
+                 d20230608,
+                 d20230626,
+                 d20230708,
+                 d20230726,
+                 d20230729,
+                 d20230807,
+                 d20230808,
+                 d20230914,
+                 d20230922,
+                 d20231003)
+
+# Kluane ###
+d20220404 <- list.files('../../data/nasa-hls/kluane/s30/2022-04-04/', full.names = TRUE)
+d20220512 <- list.files('../../data/nasa-hls/kluane/s30/2022-05-12/', full.names = TRUE)
+d20220529 <- list.files('../../data/nasa-hls/kluane/s30/2022-05-29/', full.names = TRUE)
+d20220608 <- list.files('../../data/nasa-hls/kluane/s30/2022-06-08/', full.names = TRUE)
+d20220708 <- list.files('../../data/nasa-hls/kluane/s30/2022-07-08/', full.names = TRUE)
+d20220721 <- list.files('../../data/nasa-hls/kluane/s30/2022-07-21/', full.names = TRUE)
+d20220726 <- list.files('../../data/nasa-hls/kluane/s30/2022-07-26/', full.names = TRUE)
+d20220812 <- list.files('../../data/nasa-hls/kluane/s30/2022-08-12/', full.names = TRUE)
+d20220916 <- list.files('../../data/nasa-hls/kluane/s30/2022-09-16/', full.names = TRUE)
+d20220924 <- list.files('../../data/nasa-hls/kluane/s30/2022-09-24/', full.names = TRUE)
+d20220929 <- list.files('../../data/nasa-hls/kluane/s30/2022-09-29/', full.names = TRUE)
+d20221006 <- list.files('../../data/nasa-hls/kluane/s30/2022-10-06/', full.names = TRUE)
+
+
+# List of imagery dates, for later use
+# Blaesedalen
+dates <- c('2023-04-06',
+           '2023-05-01', 
+           '2023-05-16',
+           '2023-05-22', 
+           '2023-06-08', 
+           '2023-06-26', 
+           '2023-07-08', 
+           '2023-07-26', 
+           '2023-07-29', 
+           '2023-08-07', 
+           '2023-08-08', 
+           '2023-09-14', 
+           '2023-09-22', 
+           '2023-10-03')
+
+# Kluane
+dates <- c('2022-04-04',
+           '2022-05-12',
+           '2022-05-29',
+           '2022-06-08',
+           '2022-07-08',
+           '2022-07-21',
+           '2022-07-26',
+           '2022-08-12',
+           '2022-09-16',
+           '2022-09-24', 
+           '2022-09-29', 
+           '2022-10-06')
+
+# Get the relevant HLS bands as raster stack, project + crop to extent of UAV imagery
+
+# Create list where each item in the list is another list,
+#   containing the filepath to each imagery band
+
+# Blaesedalen
+s30.data <- list(d20230406,
+                d20230501,
+                d20230516,
+                d20230522,
+                d20230608,
+                d20230626,
+                d20230708,
+                d20230726,
+                d20230729,
+                d20230807,
+                d20230808,
+                d20230914,
+                d20230922,
+                d20231003)
+
+# Kluane
+s30.data <- list(d20220404,
+                d20220512,
+                d20220529,
+                d20220608,
+                d20220708,
+                d20220721,
+                d20220726,
+                d20220812,
+                d20220916,
+                d20220924, 
+                d20220929, 
+                d20221006)
+
+# Get uAV imagery over plot to use for cropping - RE-EXPORT UAV IMAGERY SO RE-PROJECT IS AVOIDED
+# Blaesedalen
+uav <- rast('../../data/uav/orthomosaics/m3m/5cm/2023-07-02-5cm-clipped.tif')
+# Kluane-low
+uav <- rast('../../data/uav/orthomosaics/maia/kluane-low/5cm/2022-07-05-5cm-clipped.tif')
+# Kluane-high
+uav <- rast('../../data/uav/orthomosaics/maia/kluane-high/5cm/2022-07-09-5cm-clipped.tif')
+
+# Create function for stacking rasters from lists of filepaths, then cropping to extent of UAV imagery
+import_s30 <- function(x) {
+  x <- rast(x[1:13]) %>%
+    crop(uav)
+  #x <- set.names(object = x, nm = c('aot', 'blue', 'green', 'red', 'nir', 'tci1', 'tci2', 'tci3', 'wvp'))
+}
+
+# Import the S30 data using function
+s30.data.import <- pblapply(s30.data, import_s30)
+
+# Function to calculate NDVI (B8A = narrow NIR, B4 = red)
+s30_ndvi <- function(x) {
+  x <- (x$NIR_Narrow-x$Red)/(x$NIR_Narrow+x$Red)
+}
+
+# Apply function to stacked S30 raster list
+s30.ndvi <- pblapply(s30.data.import, s30_ndvi)
+
+# Stack NDVI rasters into single spatRast
+s30.ndvi <- rast(s30.ndvi)
+
+# Name raster layers with dates
+names(s30.ndvi) <- dates
+
+# Plot for sanity check
+plot(s30.ndvi)
+
+# Extract raster time series to points
+s30.ndvi.points <- st_as_sf(as.points(s30.ndvi, values = TRUE)) %>%
+  mutate(id = row_number())
+
+# Write out the extracted points
+st_write(s30.ndvi.points, '../../data/nasa-hls/output/s30-kluane-high-ndvi-ts-pt-2023.csv', 
+         layer_options = "GEOMETRY=AS_XY")
+
+
+###
+# PART 2
+###
+
+# Apply smoothed spline to every pixel time series ----
+
+# If Part 1 of this script has not been run, read in the data
+s30.ndvi.points <- read.csv('../../data/nasa-hls/s30/output/s30-ndvi-ts-pt-2023.csv') %>%
+  st_as_sf(coords = c('X', 'Y'), crs = 32621)
+
+# Get a dataframe of points from the raster
+s30.ndvi.long <- s30.ndvi.points %>%
+  pivot_longer(!geometry & !id, names_to = 'doy', values_to = 'ndvi') %>%
+  mutate(#doy = sub('X', '', doy), 
+         #doy = sub('\\.', '-', doy), 
+         doy = as_date(doy),
+         doy = yday(doy)) %>%
+  group_by(id)
+
+# Filter out obs with ndvi < 0.1, and groups where there are less than 5 obs
+s30.ndvi.long <- s30.ndvi.long %>% 
+  #filter(ndvi >= 0.1) %>%
+  filter(n_distinct(doy) >= 5)
+
+
+# Function for fitting smoothed spline model
+model_fit <- function(df) {
+  # Using a spline smoother
+  smooth.spline(x = df$doy, y = df$ndvi, spar = 0.5)
+}
+
+# Function for calculation of vertex
+# https://quantifyinghealth.com/plot-a-quadratic-function-in-r/
+find_vertex = function(model_fit) {
+  # Get model coefficients
+  a = model_fit$coefficients[3]
+  b = model_fit$coefficients[2]
+  c = model_fit$coefficients[1]
+  # Determine x coordinate of vertex
+  vertex_x = -b / (2 * a)
+  # Calculate y coordinate of vertex
+  vertex_y = a * vertex_x**2 + b * vertex_x + c
+  # Strip attributes and return as data.frame
+  return(data.frame(
+    x = as.numeric(vertex_x),
+    y = as.numeric(vertex_y)
+  ))
+}
+
+# Define function to model, find vertex, and precict values
+model_ndvi <- function(data) {
+  
+  ### This doesn't work, have filtered above instead
+  # Filter the dataset, to retain only records where NDVI >= 0.1
+  # data <- data %>%
+  #  filter(ndvi >= 0.1) %>%
+  #  filter(n_distinct(doy) >= 5)
+  
+  # Use function to fit model
+  model <- model_fit(data)
+  
+  # Generate predictions for curve plotting (for time-period doy 130-300)
+  pred <- predict(model, data.frame(doy = 130:300))
+  # pred <- unlist(pred$y)
+  
+  # use function to find vertex (linear model)
+  # vertex <- find_vertex(model)
+  # find vertex based on predictions (spline smoother)
+  vertex <- data.frame(
+    x = pred$x[pred$y == max(pred$y)], 
+    y = pred$y[pred$y == max(pred$y)]
+  )
+  
+  # Write necessary values back to df
+  data <- suppressMessages(full_join(data, data.frame(
+    doy = 130:300,
+    ndvi.max = vertex$y,
+    ndvi.max.doy = vertex$x,
+    ndvi.pred = pred
+  ))) %>% 
+    # add missing geometries
+    mutate(
+      geometry = st_geometry(data[1, ])
+    )
+  return(data)
+}
+
+# Following tutorial here: https://data-se.netlify.app/2018/12/10/new-split-apply-combine-variant-in-dplyr-group-split/
+
+# Apply model_ndvi to data using group_modify
+s30.modelled.ndvi <- s30.ndvi.long %>%
+  group_modify(~ model_ndvi(.x)) %>%
+  mutate(ndvi.max.date = as_date(ndvi.max.doy),
+         # Calculate fractional day of year, by adding decimal to date
+         ndvi.max.doy = yday(ndvi.max.date) + (ndvi.max.doy - floor(ndvi.max.doy)))
+
+# Quick quality control plots
+# 100 random pixels overview
+ggplot(
+  s30.modelled.ndvi %>% filter(id %in% sample(unique(s30.modelled.ndvi$id), 100)),
+  aes(x = doy, colour = id, group = id)
+) +
+  geom_point(aes(y = ndvi)) +
+  geom_line(aes(y = ndvi.pred.doy.1)) +
+  theme_classic() +
+  theme(legend.position = "none")
+
+# 9 random pixels in detail
+rand_id <- sample(s30.modelled.ndvi %>% st_drop_geometry() %>% pull(id) %>% unique(), 9)
+#rand_id <- c(172, 343, 493, 839, 884, 1026, 1097, 1139, 1165)
+ggplot(
+  s30.modelled.ndvi %>% filter(id %in% rand_id),
+  aes(x = doy, group = id)
+) +
+  geom_point(aes(y = ndvi)) +
+  geom_line(aes(y = ndvi.pred.doy.1)) +
+  geom_point(aes(x = ndvi.max.doy, y = ndvi.max), color = "red") +
+  facet_wrap(~id) +
+  theme_classic()
+
+# Outputs ---- 
+
+# Wide format
+s30.modelled.export.wide <- s30.modelled.ndvi %>%
+  group_by(id) %>%
+  filter(doy == 220) %>%
+  dplyr::select(-doy, -ndvi.pred.doy.1, -ndvi.max.date, -ndvi, -ndvi.pred.doy)
+
+st_write(st_as_sf(s30.modelled.export.wide),  '../../data/nasa-hls/output/s30_kluane-high-modelled_smoothed_spline_point_wide.csv', 
+         layer_options = "GEOMETRY=AS_XY")
+
+# Long format
+s30.modelled.export.long <- s30.modelled.ndvi %>%
+  rename(doy.obs = 'doy', 
+         ndvi.obs = 'ndvi', 
+         ndvi.pred = 'ndvi.pred.doy.1') %>%
+  select(-ndvi.max.date)
+
+st_write(st_as_sf(s30.modelled.export.long),  '../../data/nasa-hls/output/s30_kluane-high-modelled_smoothed_spline_point_long.csv', 
+         layer_options = "GEOMETRY=AS_XY")
