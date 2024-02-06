09fd1e7b6e6e0e8ff394d68dd76f624b2108553d
CalumHd <calum.hoad@ed.ac.uk>
2024-02-06 18:49:55 +0000

    output fig 5

--- scripts/r/plot-5a-blaesedalen-map-trio.R
+++ scripts/r/plot-5a-blaesedalen-map-trio.R
@@ -21,6 +21,14 @@ s2.bl <- read.csv('../../data/combined-ndvi-snow/s2-bl-smooth-joined.csv') %>%
   ungroup() %>%
   st_buffer(dist = 5, endCapStyle = "SQUARE")
 
+# Blaesedalen S30
+s30.bl <- read.csv('../../data/combined-ndvi-snow/s30-bl-smooth-joined.csv') %>%
+  st_as_sf(coords = c('X', 'Y'), crs= 32621) %>%
+  group_by(id) %>%
+  filter(row_number() == 7) %>%
+  ungroup() %>%
+  st_buffer(dist = 15, endCapStyle = "SQUARE")
+
 # Maps ----
 
 ndvi.max <- ggplot() +
@@ -29,16 +37,27 @@ ndvi.max <- ggplot() +
 
 snow.auc <- ggplot() +
   geom_sf(data = s2.bl, aes(fill = snow.auc)) +
-  scale_fill_viridis_c()
+  scale_fill_viridis_c() +
+  theme_void()
 
 ndvi.max.doy <- ggplot() +
   geom_sf(data = s2.bl, aes(fill = ndvi.max.doy)) +
-  scale_fill_viridis_c()
+  scale_fill_viridis_c() +
+  theme_void()
 
-combined <- plot_grid(snow.auc, 
-                      ndvi.max,
-                      ndvi.max.doy,
+s30.ndvi.max.doy <- ggplot() +
+  geom_sf(data = s30.bl, aes(fill = ndvi.max.doy)) +
+  scale_fill_viridis_c() +
+  theme_void()
+
+combined <- plot_grid(ndvi.max.doy,
+                      snow.auc, 
+                      s30.ndvi.max.doy,
                       ncol = 3, 
                       align = 'h')
 
 combined
+
+cowplot::save_plot('../../plots/figures/figure-5av1.png', combined, base_height = 140, base_width = 180, units = 'mm')
+
+s30.ndvi.max.doy
