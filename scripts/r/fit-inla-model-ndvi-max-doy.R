9e02890f1dfb437a8fd018d1cf628d926d7fdc9f
CalumHd <calum.hoad@ed.ac.uk>
2024-04-18 23:59:35 +0100

    running final inla models, plotting outputs

--- scripts/r/fit-inla-model-ndvi-max-doy.R
+++ scripts/r/fit-inla-model-ndvi-max-doy.R
@@ -10,6 +10,7 @@ library(pbapply)
 library(broom)
 library(INLA)
 library(rlang)
+library(stargazer)
 
 # Functions ----
 ## Helper function to generate INLA grid
@@ -51,7 +52,7 @@ get_inla_grid <- function(snow.auc_df, grid.size){
 fit_variogram <- function(input.data, var, grid.size, cutoff.dist, model.type) {
   
   vario <- as_Spatial(input.data) %>% as("SpatialPointsDataFrame") %>%
-    variogram(ndvi.max ~ 1, data = ., cutoff = cutoff.dist, width = grid.size)
+    variogram(ndvi.max.doy ~ 1, data = ., cutoff = cutoff.dist, width = grid.size)
   vario.max_fit <- fit.variogram(vario, model = vgm(model = model.type)) 
   
   
@@ -70,7 +71,7 @@ fit_variogram <- function(input.data, var, grid.size, cutoff.dist, model.type) {
     scale_x_continuous(limits = c(grid.size, cutoff.dist), breaks = seq(grid.size,
                                                                         cutoff.dist,
                                                                         grid.size)) +
-    labs(x = "lag distance (m)", y = "ndvi.max") +
+    labs(x = "lag distance (m)", y = "ndvi.max.doy") +
     theme_cowplot() +
     theme(axis.text.x = element_text(angle = 90, hjust = 1))
   
@@ -101,7 +102,7 @@ fit_matern <- function(shape_param, scale_param, range_site, site_data, return_m
   log.range = list(initial = log(range_site), fixed = TRUE)
   hyperpar_matern = list(initial = 2, param = c(shape_param, scale_param))
   # Specify formula
-  formula_matern <- ndvi.max ~ snow.auc +
+  formula_matern <- ndvi.max.doy ~ snow.auc +
     f(node, 
       model = "matern2d", 
       nrow = n_row,
@@ -158,11 +159,11 @@ plot_results <- function(model_matern, site_data, grid.size){
   
   # Add model fitted values and residuals to data frame
   site_data$fitted <- model_matern$summary.fitted.values$mean
-  site_data$residuals <- site_data$ndvi.max - site_data$fitted
+  site_data$residuals <- site_data$ndvi.max.doy - site_data$fitted
   
   # Plot model fit
   fit_plot <- ggplot(data = site_data) +
-    geom_point(aes(x = snow.auc, y= ndvi.max)) +
+    geom_point(aes(x = snow.auc, y= ndvi.max.doy)) +
     geom_ribbon(aes(x = snow.auc, ymin = min_ndvi_pred,
                     ymax = max_ndvi_pred),
                 data = preds_credible,
@@ -241,10 +242,10 @@ s30.bl <- get_inla_grid(s30.bl, grid.size = 30)
 # Get range for NDVI max from matern variogram at each site
 
 # Fit variograms, with cutoff set to 1/3 the diagonal of the dataset
-fit_variogram(s2.bl, model.type = 'Mat', grid.size = 10, cutoff.dist = get_diagonal(s2.bl)/3) # 64.2
-fit_variogram(s2.kl, model.type = 'Mat', grid.size = 10, cutoff.dist = get_diagonal(s2.kl)/3) # 61.7
-fit_variogram(s2.kh, model.type = 'Mat', grid.size = 10, cutoff.dist = get_diagonal(s2.kh)/3) # 36.9
-fit_variogram(s30.bl, model.type = 'Mat', grid.size = 10, cutoff.dist = get_diagonal(s30.bl)/3)# 125
+fit_variogram(s2.bl, model.type = 'Mat', grid.size = 10, cutoff.dist = get_diagonal(s2.bl)/3) # 31.3
+fit_variogram(s2.kl, model.type = 'Mat', grid.size = 10, cutoff.dist = get_diagonal(s2.kl)/3) # 52.2
+fit_variogram(s2.kh, model.type = 'Mat', grid.size = 10, cutoff.dist = get_diagonal(s2.kh)/3) # 30.6
+fit_variogram(s30.bl, model.type = 'Mat', grid.size = 10, cutoff.dist = get_diagonal(s30.bl)/3)# 21.8
 
 
 # Set empirical ranges from variograms above
@@ -252,10 +253,10 @@ ranges_df <- data.frame(site = c("s2.bl",
                                  "s2.kl",
                                  "s2.kh",
                                  "s30.bl"),
-                        range = c(round(64.2/10),
-                                  round(61.7/10),
-                                  round(36.9/10), 
-                                  round(125/30))
+                        range = c(round(31.3/10),
+                                  round(52.2/10),
+                                  round(30.6/10), 
+                                  round(21.8/30))
 )
 
 
@@ -277,12 +278,9 @@ s2.bl.sensitivity <- map(scale_param, function(x){
            range_site = ranges_df$range[1], 
            site_data = s2.bl) %>% bind_rows()
 }) %>% bind_rows()
-write_csv(s2.bl.sensitivity, "../../data/statistical-output/s2-BL-ndvi-max-prior-sensitivity.csv")
+write_csv(s2.bl.sensitivity, "../../data/statistical-output/s2-BL-ndvi-doy-prior-sensitivity.csv")
 
-# => This model is somehwat sensitive to the hyperparameter choice!
-# Optimum paprameter values will estimate the range well
-# We're aiming for a range around 5 cells -> choose scale of 0.1 and shape of
-# 100, there seems to be the least sensitivity for the range in that area. 
+# This model is insensitive to hyperparameter choice
 
 # Fit models for s2.kl
 s2.kl.sensitivity <- map(scale_param, function(x){
@@ -291,7 +289,7 @@ s2.kl.sensitivity <- map(scale_param, function(x){
            range_site = ranges_df$range[2], 
            site_data = s2.kl) %>% bind_rows()
 }) %>% bind_rows()
-write_csv(s2.kl.sensitivity, "../../data/statistical-output/s2-KL-ndvi-max-prior-sensitivity.csv")
+write_csv(s2.kl.sensitivity, "../../data/statistical-output/s2-KL-ndvi-doy-prior-sensitivity.csv")
 # This model is pretty consistent in the effect estimation across hyperparameters
 # We aim for a range of around 6 cells, this seems to be most stable around  
 # scale = 0.01 and shape = 75
@@ -303,7 +301,7 @@ s2.kh.sensitivity <- map(scale_param, function(x){
            range_site = ranges_df$range[3], 
            site_data = s2.kh) %>% bind_rows()
 }) %>% bind_rows() 
-write_csv(s2.kh.sensitivity, "../../data/statistical-output/s2-KH-ndvi-max-prior-sensitivity.csv")
+write_csv(s2.kh.sensitivity, "../../data/statistical-output/s2-KH-ndvi-doy-prior-sensitivity.csv")
 # Again fairly stable, we're aimaing for a range of 4, so scale = 0.1 and shape =
 # 400 seems to be best fitting
 
@@ -314,14 +312,14 @@ s30.bl.sensitivity <- map(scale_param, function(x){
            range_site = ranges_df$range[4], 
            site_data = s30.bl) %>% bind_rows()
 }) %>% bind_rows() 
-write_csv(s30.bl.sensitivity, "../../data/statistical-output/s30-BL-ndvi-max-prior-sensitivity.csv")
+write_csv(s30.bl.sensitivity, "../../data/statistical-output/s30-BL-ndvi-doy-prior-sensitivity.csv")
 # => Very consitent reuslts, aim for 11 cells -> scale 0.1, shape 75
 
 #### Now remove all model data on the hard drive!!! (do that manually for safety)
 #list.dirs() 
 
 # Fit final models ----
-s2.bl_fit <- fit_matern(scale_param = 0.1,
+s2.bl_fit <- fit_matern(scale_param = 0.01,
                         shape_param = 1,
                         range_site = ranges_df$range[1], 
                         site_data = s2.bl,
@@ -351,149 +349,23 @@ plot_results(s2.kh_fit, s2.kh, 10)
 plot_results(s30.bl_fit, s30.bl, 30)
 
 
-### Alternative for S2 BL: fit a break point model (manually identifited as snow.auc = 5)
+# S2 KH has a very low intercept compared with the data,
+# try thinning out the 0 snow.auc values to reduce their weight on the model
 
-# Split data into two
-s2.bl_snow_auc_lte5 <- s2.bl %>% filter(snow.auc <= 5) 
-s2.bl_snow_auc_gt5 <- s2.bl %>% filter(snow.auc > 5) 
-
-# Fit two models lte 5 first with out any modifications
-s2.bl_fit_lte5 <- fit_matern(scale_param = 0.01,
-                             shape_param = 1,
-                             range_site = ranges_df$range[1], 
-                             site_data = s2.bl_snow_auc_lte5,
-                             return_model = T) 
-summary(s2.bl_fit_lte5)
-
-# gt5 next with fixed intercept prior from s2.bl_fit_lte5 model
-
-# Calculate prediction for 5 for first model
-intercept_gt5 <- s2.bl_fit_lte5$summary.fixed[1,1] + 5 * s2.bl_fit_lte5$summary.fixed[2,1]
-
-# Remove intercept from data and remove offest from 0 on x axis
-s2.bl_snow_auc_gt5$snow.auc <- s2.bl_snow_auc_gt5$snow.auc - 5
-
-# Define max number of rows and cols
-n_col = max(s2.bl$col_number)
-n_row = max(s2.bl$row_number)
-
-# Specify priors for hyperparemeter
-log.range = list(initial = log(ranges_df$range[1]), fixed = TRUE)
-hyperpar_matern = list(initial = 2, param = c(1,  0.01))
-
-# Specify formula (no intercept)
-formula_matern <- ndvi.max ~ snow.auc +
-  f(node, 
-    model = "matern2d", 
-    nrow = n_row,
-    ncol = n_col,
-    hyper = list(range = log.range,
-                 prec = hyperpar_matern))
-# Fit model
-s2.bl_fit_gt5 <- inla(formula = formula_matern, 
-                      data = s2.bl_snow_auc_gt5,
-                      family = "gaussian",
-                      # Define prior for intercept with very high precision value
-                      control.fixed = list(mean.intercept = intercept_gt5,
-                                           prec.intercept = 0.1),
-                      control.predictor = list(compute = TRUE),
-                      control.compute = list(dic = TRUE, 
-                                             waic = TRUE, 
-                                             cpo = TRUE, 
-                                             return.marginals.predictor = TRUE),
-                      keep = TRUE)
-
-# Get the summary
-summary(s2.bl_fit_gt5)  
-
-# Calculate marginal predictions depending on snow.auc only
-s2.bl_snow_auc_lte5$preds <- s2.bl_fit_lte5$summary.fixed[1,1] + 
-  s2.bl_snow_auc_lte5$snow.auc * s2.bl_fit_lte5$summary.fixed[2,1]
-s2.bl_snow_auc_gt5$preds <- s2.bl_fit_gt5$summary.fixed[1,1] + 
-  s2.bl_snow_auc_gt5$snow.auc * s2.bl_fit_gt5$summary.fixed[2,1]
-
-# Calculate min and max values for 95 CI predictions to plot a ribbon
-preds_credible_lte5 <- expand.grid(intercept = unlist(s2.bl_fit_lte5$summary.fixed[1, c(1,3,5)]), 
-                                   slope = unlist(s2.bl_fit_lte5$summary.fixed[2, c(1,3,5)])) %>%
-  split(1:nrow(.)) %>%
-  map(function(parameters) {
-    data.frame(snow.auc =  seq((floor(min(s2.bl_snow_auc_lte5$snow.auc) * 10) / 10),
-                               (ceiling(max(s2.bl_snow_auc_lte5$snow.auc) * 10) / 10), 0.01),
-               ndvi_pred = parameters$intercept[1] + seq((floor(min(s2.bl_snow_auc_lte5$snow.auc) * 10) / 10),
-                                                         (ceiling(max(s2.bl_snow_auc_lte5$snow.auc) * 10) / 10), 0.01) * parameters$slope[1])
-  }) %>% bind_rows() %>%
-  group_by(snow.auc) %>% summarise(min_ndvi_pred = min(ndvi_pred), max_ndvi_pred = max(ndvi_pred))
-preds_credible_gt5 <- expand.grid(intercept = unlist(s2.bl_fit_gt5$summary.fixed[1, c(1,3,5)]), 
-                                  slope = unlist(s2.bl_fit_gt5$summary.fixed[2, c(1,3,5)])) %>%
-  split(1:nrow(.)) %>%
-  map(function(parameters) {
-    data.frame(snow.auc =  seq((floor(min(s2.bl_snow_auc_gt5$snow.auc) * 10) / 10),
-                               (ceiling(max(s2.bl_snow_auc_gt5$snow.auc) * 10) / 10), 0.01),
-               ndvi_pred = parameters$intercept[1] + seq((floor(min(s2.bl_snow_auc_gt5$snow.auc) * 10) / 10),
-                                                         (ceiling(max(s2.bl_snow_auc_gt5$snow.auc) * 10) / 10), 0.01) * parameters$slope[1])
-  }) %>% bind_rows() %>%
-  group_by(snow.auc) %>% summarise(min_ndvi_pred = min(ndvi_pred), max_ndvi_pred = max(ndvi_pred)) %>% 
-  mutate(snow.auc = snow.auc + 5)
-
-# Add model fitted values and residuals to data frame
-s2.bl_snow_auc_lte5$fitted <- s2.bl_fit_lte5$summary.fitted.values$mean
-s2.bl_snow_auc_lte5$residuals <- s2.bl_snow_auc_lte5$ndvi.max - s2.bl_snow_auc_lte5$fitted
-s2.bl_snow_auc_gt5$fitted <- s2.bl_fit_gt5$summary.fitted.values$mean 
-s2.bl_snow_auc_gt5$residuals <- s2.bl_snow_auc_gt5$ndvi.max - s2.bl_snow_auc_gt5$fitted
-
-# Re-add intercept to ndvi.max for gt5 and shift data on x axis
-s2.bl_snow_auc_gt5$snow.auc <- s2.bl_snow_auc_gt5$snow.auc + 5
+# # Quick and dirty subsampling for KH site
+hist(s2.kh.thin$snow.auc, breaks = seq(-0.5, 23.5,1))
+s2.kh$bin <- round(s2.kh$snow.auc)
+group_by(s2.kh, bin) %>% st_drop_geometry() %>% tally()
+group_by(s2.kh, bin) %>% st_drop_geometry() %>% tally() %>% summarize(mean(n))
+s2.kh.thin <- group_by(s2.kh, bin) %>% group_map(function(x, ...) {
+  if(nrow(x) >= 20) x <- sample_n(x, 350)
+  return(x)
+}) %>% bind_rows()
+s2.kh.thin %>% mutate(bin = round(snow.auc)) %>%
+  group_by(bin) %>% st_drop_geometry() %>% tally()
 
 
-# Plot model fits
-bl_plot <- ggplot() +
-  geom_point(data = s2.bl_snow_auc_lte5, aes(x = snow.auc, y= ndvi.max), colour = '#BECBE7') +
-  geom_point(data = s2.bl_snow_auc_gt5, aes(x = snow.auc, y= ndvi.max), colour ='#BECBE7') +
-  geom_ribbon(aes(x = snow.auc, ymin = min_ndvi_pred,
-                  ymax = max_ndvi_pred),
-              data = preds_credible_lte5,
-              fill = "#4984BF", alpha = 0.5) +
-  geom_ribbon(aes(x = snow.auc, ymin = min_ndvi_pred,
-                  ymax = max_ndvi_pred),
-              data = preds_credible_gt5,
-              fill = '#4984BF', alpha = 0.5) +
-  geom_line(aes(x = snow.auc, y = preds), colour = '#4984BF', data = s2.bl_snow_auc_lte5) +
-  geom_line(aes(x = snow.auc, y = preds), colour = '#4984BF', data = s2.bl_snow_auc_gt5) +
-  theme_cowplot()
-bl_plot
-
-# Residual plots
-hist_lte5 <- ggplot(s2.bl_snow_auc_lte5) +
-  geom_histogram(aes(x = residuals), bins = 50) +
-  labs(x = "residuals") +
-  theme_cowplot()
-hist_gt5 <- ggplot(s2.bl_snow_auc_gt5) +
-  geom_histogram(aes(x = residuals), bins = 50) +
-  labs(x = "residuals") +
-  theme_cowplot()
-plot_grid(hist_lte5, hist_gt5, ncol = 2, labels = c('lte5', 'gt5'))
-
-resids_space <- ggplot() +
-  geom_sf(data = st_buffer(s2.bl_snow_auc_lte5, 5, endCapStyle = "SQUARE"), aes(fill = residuals)) +
-  geom_sf(data = st_buffer(s2.bl_snow_auc_gt5, 5, endCapStyle = "SQUARE"), aes(fill = residuals)) +
-  scale_fill_continuous_diverging() +
-  theme_map()
-resids_space
-
-# Plot predictions in space
-preds_in_space <- ggplot() +
-  geom_sf(data = st_buffer(bind_rows(s2.bl_snow_auc_lte5, s2.bl_snow_auc_gt5), 5, endCapStyle = "SQUARE"), 
-          aes(fill = preds)) +
-  scale_fill_continuous_sequential("viridis", rev = F) +
-  labs(fill = "predictions\nNDVI max") +
-  theme_map()
-data_in_space <- ggplot() +
-  geom_sf(data = st_buffer(s2.bl, 5, endCapStyle = "SQUARE"), 
-          aes(fill = ndvi.max)) +
-  scale_fill_continuous_sequential("viridis", rev = F) +
-  labs(fill = "true\nNDVI max") +
-  theme_map()
-plot_grid(preds_in_space, data_in_space)
+s2.kh.thin <- s2.kh
 
 
 # Final plots ----
@@ -525,7 +397,8 @@ fit_plot <- function(model_matern, site_data, grid.size,
   
   # Plot model fit
   fit_plot <- ggplot(data = site_data) +
-    geom_point(aes(x = snow.auc, y= ndvi.max), colour = colour.lightest) +
+    geom_point(aes(x = snow.auc, y= ndvi.max.doy), colour = colour.lightest) +
+    #geom_point(data = site_data %>% distinct(snow.auc, ndvi.max.doy), aes(x = snow.auc, y= ndvi.max.doy), colour = 'black', alpha = 0.1) +
     geom_ribbon(aes(x = snow.auc, ymin = min_ndvi_pred,
                     ymax = max_ndvi_pred),
                 data = preds_credible,
@@ -537,27 +410,35 @@ fit_plot <- function(model_matern, site_data, grid.size,
   return(fit_plot)
 }
 
-
+bl <- fit_plot(s2.bl_fit, s2.bl, 10, 
+               colour.site = '#4984BF', 
+               colour.darker = '#2E5277',
+               colour.lighter = '#9BB2DA',
+               colour.lightest = '#BECBE7', 
+               ymax = 240, ymin = 215)
 kl <- fit_plot(s2.kl_fit, s2.kl, 10, 
                colour.site = '#F5A40C', 
                colour.darker = '#946606',
                colour.lighter = '#FBCA7F',
                colour.lightest = '#FDDCAC', 
-               ymax = 0.7, ymin = 0.4)
+               ymax = 235, ymin = 210)
 kh <- fit_plot(s2.kh_fit, s2.kh, 10, 
                colour.site = '#F23835', 
                colour.darker = '#8D271E',
                colour.lighter = '#F29580',
                colour.lightest = '#F8BBAA', 
-               ymax = 0.6, ymin = 0.15)
+               ymax = 235, ymin = 210)
 
+# Create panel plot
 top <- plot_grid(kl, kh, ncol = 2, align = 'h')
-bottom <- plot_grid(bl_plot, bl_plot, ncol = 2, align = 'h')
+bottom <- plot_grid(bl, bl, ncol = 2, align = 'h')
 combined <- plot_grid(top, bottom, nrow = 2, align = 'v')
-combined
+
+#Check the plot
+combined 
 
 # Save plots
-cowplot::save_plot('../../plots/figures/figure-3-inla-v2.png', combined, 
+cowplot::save_plot('../../plots/figures/figure-4-inla.png', combined, 
                    base_height = 140, base_width = 180, units = 'mm', 
                    bg = 'white')
 
@@ -567,27 +448,9 @@ s30.bl.plot <- fit_plot(s30.bl_fit, s30.bl, 10,
                         colour.darker = '#2E5277',
                         colour.lighter = '#9BB2DA',
                         colour.lightest = '#BECBE7', 
-                        ymax = 0.8, ymin = 0.45)
+                        ymax = 240, ymin = 210)
 s30.bl.plot
 
-cowplot::save_plot('../../plots/figures/figure-5-ndvimax.png', s30.bl.plot, 
+cowplot::save_plot('../../plots/figures/figure-5-ndvimaxdoy.png', s30.bl.plot, 
                    base_height = 70, base_width = 90, units = 'mm', 
                    bg = 'white')
-
-
-# Get summary from S2 BL break-point model
-model_matern <- s2.bl_fit_gt5
-site_data <- s2.bl_snow_auc_gt5
-
-data.frame(scale_param = 0.01,
-           shape_param = 1, 
-           intercept = round(model_matern$summary.fixed$mean[1],5),
-           slope = round(model_matern$summary.fixed$mean[2],5),
-           slope_q025 = round(model_matern$summary.fixed[2,3],5),
-           slope_q975 = round(model_matern$summary.fixed[2,5],5),
-           range = round(model_matern$summary.hyperpar$mean[3],2), 
-           dic = model_matern$dic$dic,
-           dic_eff_par = model_matern$dic$p.eff,
-           dic_d_p = nrow(site_data)/model_matern$dic$p.eff,
-           waic = model_matern$waic$waic,
-           waic_eff_par = model_matern$waic$p.eff)
